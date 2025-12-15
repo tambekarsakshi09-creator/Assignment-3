f39311b3046f6b78ca00cdbeb35a993e782c4492
SakshiT <tambekarsakshi09@gmail.com>
2025-12-15 15:56:45 +0530

    Add files via upload

---
+++ A3 Task 2.py
@@ -0,0 +1,14 @@
+import math
+
+# Taking input from the user
+num = float(input("Enter a number: "))
+
+# Calculations using math module
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+# Displaying results
+print("Square root:", square_root)
+print("Natural logarithm:", natural_log)
+print("Sine value (in radians):", sine_value)
